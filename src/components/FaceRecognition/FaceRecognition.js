c24081b2717133a09c817323cae35b2a468b724c
Daniel <62781672+dwernz@users.noreply.github.com>
2023-04-24 09:39:46 -0400

    Update: Deployment test

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import './FaceRecognition.css';
 
-const FaceRecognition = () => {
+const FaceRecognition = ({ imageUrl, box }) => {
     return (
-        <h1>Face Recognition</h1>
+        <div className='center ma'>
+            <div className='absolute mt4'>
+                <img id='inputimage' src={imageUrl} alt='' width='500px' height='auto' />
+                <div className='bounding-box' style={{ top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol }}></div>
+            </div>
+        </div>
     );
 }
 
